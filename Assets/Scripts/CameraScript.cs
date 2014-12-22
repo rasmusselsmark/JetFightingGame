b64b8e762a7ca2e5e7e68ef6cab9dc2b409e508c
rasmusselsmark <rasmus@selsmark.dk>
2014-12-22 22:17:23 +0100

    Initial commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+	public GameObject Follow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var camera = this.GetComponent<Transform> ();
+		var jet = Follow.GetComponent<Transform> ();
+
+		camera.position = new Vector3 (
+			jet.position.x,
+			jet.position.y,
+			camera.position.z
+		);
+	}
+}
---
+++ Assets/Scripts/JetScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JetScript : MonoBehaviour {
+	public float Speed = 1.0f;
+	public float Rotation = 0.0f;
+
+	public float MinSpeed = 1.0f;
+	public float MaxSpeed = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Turn
+		Transform t = GetComponent<Transform> ();
+		t.Rotate(0.0f, 0.0f, -Input.GetAxis ("Horizontal") * (this.Speed / 7.0f));
+
+		// Control speed
+		this.Speed += Input.GetAxis ("Vertical");
+		if (this.Speed < this.MinSpeed)
+		{
+			this.Speed = this.MinSpeed;
+		}
+		if (this.Speed > this.MaxSpeed)
+		{
+			this.Speed = this.MaxSpeed;
+		}
+
+		// Move the jet
+		t.Translate (0.0f, this.Speed * Time.deltaTime, 0.0f);
+	}
+}
