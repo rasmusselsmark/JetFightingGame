a57833d1e5fa4921d574c7c88fd925f85abd5723
rasmusselsmark <rasmus@selsmark.dk>
2014-12-25 01:48:33 +0100

    Added SceneController

---
+++ Assets/SceneControllerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneControllerScript : MonoBehaviour
+{
+	public GameObject Jet;
+	public GameObject UfoPrefab;
+
+	// Use this for initialization
+	void Start ()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			GameObject ufo = Instantiate(UfoPrefab) as GameObject;
+			ufo.GetComponent<UfoScript>().Jet = Jet;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
