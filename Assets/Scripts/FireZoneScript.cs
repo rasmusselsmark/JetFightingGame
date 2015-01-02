e0e047a3497273084a515b20fe462fd64ddcc233
Rasmus Møller Selsmark <rasmus@selsmark.dk>
2015-01-02 17:07:47 +0100

    Added 'fire zone' for mobile use

---
+++ Assets/Scripts/FireZoneScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class FireZoneScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+	private bool _isTouched = false;
+
+	public void OnPointerUp (PointerEventData eventData)
+	{
+		_isTouched = false;
+	}
+
+	public void OnPointerDown (PointerEventData eventData)
+	{
+		_isTouched = true;
+	}
+
+	public bool IsTouched()
+	{
+#if MOBILE_INPUT
+		return _isTouched;
+#else
+		return Input.GetButton("Fire1");
+#endif
+	}
+}
